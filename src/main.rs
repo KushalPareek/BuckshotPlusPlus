29888b70c152aa9ed4eae2d5479c7c6d7de94cf2
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2022-10-05 20:16:43 +0200

    feat: addition of the log system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use log::{debug, error, info, warn, LevelFilter};
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::builder()
+        .filter_level(LevelFilter::max())
+        .init();
+
+    debug!("Hello, world!");
+    error!("Hello, world!");
+    info!("Hello, world!");
+    warn!("Hello, world!");
 }
