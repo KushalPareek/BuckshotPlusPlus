ef1fa1e38d45c786616589062c03c023ee9f72e4
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2022-10-05 20:46:19 +0200

    feat: addition of the cli system

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
-use log::{debug, error, info, warn, LevelFilter};
+use log::{LevelFilter, debug};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    path: String,
+
+    #[arg(short, long)]
+    debug: Option<u8>,
+}
 
 fn main() {
     env_logger::builder()
         .filter_level(LevelFilter::max())
         .init();
 
-    debug!("Hello, world!");
-    error!("Hello, world!");
-    info!("Hello, world!");
-    warn!("Hello, world!");
+    let args = Args::parse();
+    debug!("Path: {}", args.path);
+
+    if let Some(debug) = args.debug {
+        debug!("Debug: {:?}", debug);
+    }
 }
